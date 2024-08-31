b5365dbe1ac148d34ad7e5948fc0901fd1572bb9
trev-git <htaedshararam@gmail.com>
2024-08-31 12:44:11 +0300

    I hate pagination

--- populate.py
+++ populate.py
@@ -50,6 +50,10 @@ def main():
                     sleep(2)
                 beatmapsets = api.search_beatmapsets(query, explicit_content=ossapi.BeatmapsetSearchExplicitContent.SHOW, cursor=cursor)
                 cursor = beatmapsets.cursor
+                if cursor is None:
+                    for beatmapset in beatmapsets.beatmapsets:
+                        download_beatmap(beatmapset, config['OSU_SESSION_COOKIES'])
+                        sleep(2)
 
 
 if __name__ == '__main__':
